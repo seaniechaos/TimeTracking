3c6f50c4da91b3030197473284e86aade5ddcc26
seaniechaos <seaniechaos@gmail.com>
2015-02-02 18:22:43 +0100

    Final commit of the day...

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,9 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.last_created_projects(10)
+    if @projects.empty?
+      render 'no_projects'
+    end
   end
 
   def show
