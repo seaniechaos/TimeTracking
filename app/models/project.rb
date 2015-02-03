8bd0f9c04957ae915510a5131a6253864dfa436b
seaniechaos <seaniechaos@gmail.com>
2015-02-03 11:52:26 +0100

    Added entries to my DB.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
+  has_many :entries
+  
   def self.iron_find (id)
     where(id: id).first
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Project.create name: 'Ironhack', description: 'Ironhack is a...'
-Project.create name: 'Time tracking app'
-Project.create name: 'Recipes', description: 'Track my favourite recipes'
-Project.create name: 'Sean Ring', description: 'The most interesting man in the world'
+
+m = Project.create name: 'Time tracking app'
+m.entries.create hours: 16, minutes: 25
+
+n = Project.create name: 'Recipes', description: 'Track my favourite recipes'
+n.entries.create hours: 23, minutes: 34
+
+p = Project.create name: 'Sean Ring', description: 'The most interesting man in the world'
+p.entries.create hours: 5, minutes: 32
+
+
+
