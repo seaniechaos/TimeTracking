05d59101380b37ed4afadf0c2c88c0a14cf2eff3
seaniechaos <seaniechaos@gmail.com>
2015-02-03 12:29:41 +0100

    More database exercises.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/contact' => 'site#contact'
   get '/projects' => 'projects#index'
   get '/projects/:id' => 'projects#show'
+  get '/projects/:project_id/entries' => 'entries#index'
   get '/list' => 'site#list'
 end
